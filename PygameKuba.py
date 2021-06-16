1f280b1198bb1ab70daa9ea17afc42ca9bb73194
fitzpaca <fitzpaca@oregonstate.edu>
2021-06-16 16:18:19 -0400

    learning pygame

--- PygameKuba.py
+++ PygameKuba.py
@@ -8,26 +8,63 @@ from KubaGame import KubaGame, GameBoard, Player, InvalidMoveError, Queue
 # initialize all the modules required for PyGame
 pygame.init()
 
-done = False
-screen = pygame.display.set_mode((500, 500))
-x = 60
-y = 60
+# define constants for the screen width and height
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
 
-while not done:
+# create the screen object
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# set tile information (coordinates, color)
+tile_x = 0
+tile_y = 0
+tile_color = (255, 255, 255)    # white
+
+# create object for background image
+image = pygame.image.load(r'C:\Users\Carl\Desktop\Grease.jpg')
+
+# add this before loop to throttle the frame rate
+clock = pygame.time.Clock()
+
+running = True
+while running:
     # since the game will be a series of events, this syntax will help empty the events'
     #  queue before adding a new one
     for event in pygame.event.get():
-        # this syntax is used to exit the program altogether
+        # did the user click the window close button?
         if event.type == pygame.QUIT:
-            done = True
+            running = False
+
+        # set the background to loaded image
+        screen.blit(image, (0, 0))
+
+        # change the color of the square by pressing space bar
+        is_red = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_red = not is_red
+            if is_red:
+                tile_color = (255, 0, 0)
+            else:
+                tile_color = (102, 0, 0)
 
         # start editing the output window
         pygame.display.set_caption("Kuba Board Game")       # set window title
         # icon = pygame.image.load('game.jpg')              # set title icon
         # pygame.display.set_icon(icon)
 
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]: tile_y -= 100
+        if pressed[pygame.K_DOWN]: tile_y += 100
+        if pressed[pygame.K_LEFT]: tile_x -= 100
+        if pressed[pygame.K_RIGHT]: tile_x += 100
 
-        pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 90, 90))
+        # add this in loop
+        clock.tick(60)
+
+        pygame.draw.rect(screen, tile_color, pygame.Rect(tile_x, tile_y, 100, 100))
+
+        # reset screen before drawing next rectangle
+        # screen.blit(image, (0, 0))
 
         # PyGame is double-buffered so this swaps the buffers.
         #  this call is required for any updates that you make
