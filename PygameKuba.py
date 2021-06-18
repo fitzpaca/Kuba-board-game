7f915f4d691ecb086e7f0928219aa976ee77f5d4
fitzpaca <fitzpaca@oregonstate.edu>
2021-06-18 14:15:52 -0400

    dynamic board and marble size

--- PygameKuba.py
+++ PygameKuba.py
@@ -7,7 +7,6 @@ import pygame, sys, math
 
 from pygame.locals import *
 
-import KubaGame
 from KubaGame import KubaGame
 
 ################################################################################
@@ -30,7 +29,7 @@ pygame.init()
 
 # set tile information (coordinates, color)
 tile_radius = 40
-tile_spacing = 20
+tile_spacing = 5
 
 # define constants for the screen width and height
 SCREEN_WIDTH = (8 * tile_spacing) + (7 * 2 * tile_radius)
@@ -45,10 +44,11 @@ grey = (192, 192, 192)
 # create the screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
+
 ################################################################################
 # Game Loop ####################################################################
 ################################################################################
+running = True
 while running:
     # since the game will be a series of events, this syntax will help empty the events'
     #  queue before adding a new one
@@ -61,12 +61,12 @@ while running:
         pygame.display.set_caption("Kuba Board Game")  # set window title
 
         # display all marbles on the board
-        x_counter = 60
-        y_counter = 60
+        x_counter = tile_radius + tile_spacing
+        y_counter = tile_radius + tile_spacing
         for row in game.get_board()[1:8]:
             for tile in row[1:8]:
-                if x_counter > 660:
-                    x_counter = 60
+                if x_counter > (7 * tile_spacing) + ((6 * 2 + 1) * tile_radius):
+                    x_counter = tile_radius + tile_spacing
 
                 if tile == 'W':
                     pygame.draw.circle(screen, white, (x_counter, y_counter), tile_radius)
@@ -79,10 +79,10 @@ while running:
 
                 # move across the board to the right for every tile
                 #  until it reaches 660, then reset to 0
-                x_counter += 100
+                x_counter += 2 * tile_radius + tile_spacing
 
             # move down the board row by row until y_counter reaches 600 then stop
-            y_counter += 100
+            y_counter += 2 * tile_radius + tile_spacing
 
         # did the user hit a key?
         if event.type == KEYDOWN:
@@ -95,17 +95,21 @@ while running:
             # store mouse click coordinates
             mx, my = pygame.mouse.get_pos()
 
-            for row in range(tile_radius + tile_spacing, 720, 100):
-                for col in range(60, 720, 100):
+            for row in range(tile_radius + tile_spacing,
+                             (8 * tile_spacing) + (7 * 2 * tile_radius),
+                             2 * tile_radius + tile_spacing):
+                for col in range(tile_radius + tile_spacing,
+                             (8 * tile_spacing) + (7 * 2 * tile_radius),
+                             2 * tile_radius + tile_spacing):
                     x_pos = row
                     y_pos = col
 
                     mx_sq = (mx - x_pos) ** 2
                     my_sq = (my - y_pos) ** 2
 
-                    if math.sqrt(mx_sq + my_sq) <= 40:
-                        marble_index = (int((y_pos - 60) / 100), int((x_pos - 60) / 100))
-
+                    if math.sqrt(mx_sq + my_sq) <= tile_radius:
+                        marble_index = (int((y_pos - (tile_radius + tile_spacing)) / (2 * tile_radius + tile_spacing)),
+                                        int((x_pos - (tile_radius + tile_spacing)) / (2 * tile_radius + tile_spacing)))
                         print(marble_index)
                         # output color of marble:
                         if screen.get_at((mx, my)) == white:
@@ -114,7 +118,6 @@ while running:
                             print('B')
                         elif screen.get_at((mx, my)) == red:
                             print('R')
-
 
         elif event.type == pygame.QUIT:
             running = False
